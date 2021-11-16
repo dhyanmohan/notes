6eb1eef4182f0a20f49b2f6ae081e6506e4b60e6
Dhyan Mohan <dhyanmohan2001@gmail.com>
2021-11-16 23:31:19 +0000

    change 2

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,18 +13,116 @@ class homeScreen extends StatelessWidget {
         title: Center(child: Text("STUDY MATERIALS")),
         backgroundColor: Colors.redAccent,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
+      body: ListView(
+        children: [
+          Container(
+            child: Text(
+              'SEM 1',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
             ),
-            Container(
-                decoration: BoxDecoration(color: Colors.black),
-                padding: EdgeInsets.all(100),
-                margin: EdgeInsets.only(left: 80, right: 80))
-          ],
-        ),
+            decoration: BoxDecoration(
+                color: Colors.grey[500],
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 2',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 3',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.greenAccent,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 4',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.black, borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 5',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.indigoAccent,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 6',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 7',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.yellowAccent,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60),
+          ),
+          Container(
+            child: Text(
+              'SEM 8',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+                color: Colors.amberAccent,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(100),
+            margin: EdgeInsets.only(left: 40, right: 40, top: 60, bottom: 60),
+          )
+        ],
       ),
     ));
   }
